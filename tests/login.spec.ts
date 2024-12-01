b51d2b3422594492024c1ac9e1cc48fe8a8e25c6
Ben Black <benjamin.black75@gmail.com>
2024-12-01 15:07:29 -0800

    added logging in test for parabank

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://parabank.parasoft.com/parabank/index.htm');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/ParaBank/);
-});
-
-//test('get started link', async ({ page }) => {
-  //await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  //await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-//});
+test('test', async ({ page }) => {
+    await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+    await page.locator('input[name="username"]').click();
+    await page.locator('input[name="username"]').fill('testUsername');
+    await page.locator('input[name="username"]').press('Tab');
+    await page.locator('input[name="password"]').fill('testPassword');
+    await page.getByRole('button', { name: 'Log In' }).click();
+  });
