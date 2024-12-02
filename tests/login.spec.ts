16bb44fc408baeecb316e0faa4fdabc111244215
Ben Black <benjamin.black75@gmail.com>
2024-12-02 09:59:19 -0800

    added a check to verify the next page

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+  test('has title', async ({ page }) => {
     await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/ParaBank/);
+
     await page.locator('input[name="username"]').click();
-    await page.locator('input[name="username"]').fill('testUsername');
+    await page.locator('input[name="username"]').fill('john');
     await page.locator('input[name="username"]').press('Tab');
-    await page.locator('input[name="password"]').fill('testPassword');
+    await page.locator('input[name="password"]').fill('demo');
+
+    //log in
     await page.getByRole('button', { name: 'Log In' }).click();
+
+    // Expects page to have a title with the name of Accounts Overview.
+    await expect(page).toHaveTitle(/Accounts Overview/);
   });
